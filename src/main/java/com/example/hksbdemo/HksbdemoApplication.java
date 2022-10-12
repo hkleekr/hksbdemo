ab10e7d7387ead857d5964a2b81fdffaca16f0a8
HyekyungLee <hyekyoung.lee@dkbmc.com>
2022-10-12 13:20:33 +0900

    reupdate

---
+++ src/main/java/com/example/hksbdemo/HksbdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.hksbdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HksbdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HksbdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hksbdemo/HksbdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hksbdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HksbdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
